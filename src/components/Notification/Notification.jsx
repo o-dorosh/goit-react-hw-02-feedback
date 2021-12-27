ace3a6c9bef8942ae3ea4469647984fbe0d7cf1e
Olga Dorosh <odorosh28@gmail.com>
2021-12-27 15:08:50 +0200

    add notification

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+// import s from '../App/App.module.css';
+
+const Notification = ({message}) => {
+    return(
+        <p>{message}</p>
+    );
+  };
+  
+  export default Notification;
